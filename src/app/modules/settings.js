e06933897d627cd7777b48e53559e199397eb16d
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2024-06-07 12:20:12 +0200

    Update settings.js

--- src/app/modules/settings.js
+++ src/app/modules/settings.js
@@ -1,4 +1,5 @@
-export const serverEndpoint = "https://mgp-allstars-2009-server.glitch.me/";
+export const serverEndpoint =
+	"https://psychedelic-sweltering-dracorex.glitch.me/";
 
 export const localEndpoint = "http://localhost:8080";
 
