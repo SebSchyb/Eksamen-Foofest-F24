09decdc9b9318f0ad4f68b3585211c35fc5807c1
Seb <sebastian.schyberg@gmail.com>
2024-06-11 16:14:18 +0200

    maxw

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -7,19 +7,19 @@ export default function About() {
 		<main className="text-white m-4 min-h-screen">
 			<section className="mb-8">
 				<h1 className="text-4xl font-bold mb-4">About FooFest</h1>
-				<p className="mb-4">
+				<p className="mb-4 max-w-prose">
 					FooFest is an annual celebration of heavy metal music,
 					featuring top metal bands from around the world. Its the
 					ultimate festival for metalheads, offering a diverse lineup,
 					immersive experiences, and a vibrant community.
 				</p>
-				<p className="mb-4">
+				<p className="mb-4 max-w-prose">
 					From August 12th to August 20th, join us at Refshalvøen in
 					Copenhagen, Denmark for an unforgettable experience. The
 					festival grounds offer ample space for camping, food
 					vendors, and multiple stages.
 				</p>
-				<p className="mb-4">
+				<p className="mb-4 max-w-prose">
 					Whether youre a die-hard metal fan or just looking to enjoy
 					some great music, FooFest has something for everyone. We
 					cant wait to see you there!
