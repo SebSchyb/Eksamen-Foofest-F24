82d625e563c5be6a352d60a101e409cb15a0f519
Seb <sebastian.schyberg@gmail.com>
2024-06-12 14:05:59 +0200

    deleete faq

--- src/app/booking/page.jsx
+++ src/app/booking/page.jsx
@@ -13,15 +13,6 @@ export default async function Booking() {
 	return (
 		<main className="grid p-6 md:grid-cols-2 mx-auto my-6 rounded-xl max-w-screen-lg gap-6 justify-around ">
 			<BookingArea />
-			<div className="text-white flex flex-col items-center p-4 bg-black-blue rounded-lg grow-0">
-				<article className="grow">
-					<h2 className="font-bold text-lg text-center w-full">
-						FAQ
-					</h2>
-					<FAQ />
-				</article>
-				<Image className="my-4" src={picture} alt="logo"></Image>
-			</div>
 		</main>
 	);
 }
