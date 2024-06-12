8852ae80bd2eb891bec64720cacc91fae33919cc
Seb <sebastian.schyberg@gmail.com>
2024-06-12 13:45:12 +0200

    kan ikke huske hvad jeg har ændret

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -4,8 +4,8 @@ import FAQ from "@/app/components/FAQ";
 
 export default function About() {
 	return (
-		<div className="text-white m-4 min-h-screen max-w-lg">
-			<section className="mb-8">
+		<div className="text-white m-4 min-h-screen mx-auto max-w-screen-lg grid grid-cols-2 gap-8">
+			<section className="mb-8 max-w-lg bg-black-blue p-6 rounded-2xl">
 				<h1 className="text-4xl font-bold mb-4">About FooFest</h1>
 				<p className="mb-4 max-w-prose">
 					FooFest is an annual celebration of heavy metal music,
@@ -26,7 +26,7 @@ export default function About() {
 				</p>
 			</section>
 
-			<section className="mb-8 bg-black-blue p-6 rounded-2xl max-w-lg">
+			<section className="mb-8 bg-black-blue p-6 rounded-2xl max-w-xl">
 				<h2 className="text-3xl font-bold mb-4">
 					Frequently Asked Questions
 				</h2>
