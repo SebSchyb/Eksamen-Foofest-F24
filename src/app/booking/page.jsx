9b441838ea081ee157a52ba184a67c0847a6446f
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2024-06-13 16:39:30 +0200

    Header, schedule og loading spinner fix

--- src/app/booking/page.jsx
+++ src/app/booking/page.jsx
@@ -11,7 +11,7 @@ import { apiKey, databaseTestEndport, endpoint } from "../modules/settings";
 
 export default async function Booking() {
 	return (
-		<main className="grid p-6 md:grid-cols-2 mx-auto my-6 rounded-xl max-w-screen-lg gap-6 justify-around ">
+		<main className="grid p-6 mx-auto my-6 rounded-xl max-w-screen-lg gap-6 justify-around ">
 			<BookingArea />
 		</main>
 	);
